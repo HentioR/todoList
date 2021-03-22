deedb424f21f9289104434865657728949512e58
Thomas Malassigne <21410893@unicaen.fr>
2021-03-22 04:11:51 +0100

    tasks list

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="home">
+        <ul>
+
+        </ul>
+    </div>
+</template>
+
+<script>
+/*
+    import { mapActions, mapGetters } from "vuex";
+    //import TodoList from '@/components/TodoList.vue';
+
+    export default {
+        name: 'Home',
+        components: {
+            TodoList
+        },
+        methods: {
+            ...mapActions("todolist", ['load']),
+        },
+        created() {
+            this.load()
+        },
+        computed: {
+            ...mapGetters("todolist", ['tasks'])
+        }
+    }*/
+</script>
